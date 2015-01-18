2b00b9c1ecd8b77e741dce48fe109bf884b9f7b3
madthad91 <thaddeus.madison@gmail.com>
2015-01-17 23:50:22 -0800

    fixed the bug with isMobile function

--- cloud/app.js
+++ cloud/app.js
@@ -1,18 +1,67 @@
-//app.js
 // These two lines are required to initialize Express in Cloud Code.
-require('cloud/app.js');
 var express = require('express');
 var app = express();
 
 // Global app configuration section
-app.set('views', 'cloud/views');  // Specify the folder to find templates
-app.set('view engine', 'ejs');    // Set the template engine
-app.use(express.bodyParser());    // Middleware for reading request body
+app.set('views', 'cloud/views'); // Specify the folder to find templates
+app.set('view engine', 'ejs'); // Set the template engine
+app.use(express.bodyParser()); // Middleware for reading request body
 
 // This is an example of hooking up a request handler with a specific request
 // path and HTTP verb using the Express routing API.
 app.get('/hello', function(req, res) {
-  res.render('hello', { message: 'Congrats, you just set up your app!' });
+    //res.render('hello', { message: 'Congrats, you just set up your app!' });
+
+    Parse.Cloud.httpRequest({
+        url: 'http://echo.jsontest.com/key/value/one/two',
+        followRedirects: true,
+        success: function(httpResponse) {
+            console.log(httpResponse.text);
+            res.write(httpResponse.text);
+
+            Parse.Cloud.httpRequest({
+                url: 'http://echo.jsontest.com/key/value/one/two',
+                followRedirects: true,
+                success: function(httpResponse) {
+                    res.write('The thad is back in town.');
+                    res.write(httpResponse.text);
+                    res.end();
+                },
+                error: function(httpResponse) {
+                    console.error('Request failed with response code ' + httpResponse.status);
+                    res.write('yo yo ');
+                    res.end();
+                }
+            });
+
+        },
+        error: function(httpResponse) {
+            console.error('Request failed with response code ' + httpResponse.status);
+            res.write('yo yo ');
+            res.end();
+        }
+    });
+
+
+});
+
+app.get('mail', function(req, res) {
+
+    Mailgun.sendEmail({
+        to: "email@example.com",
+        from: "Mailgun@CloudCode.com",
+        subject: "Hello from Cloud Code!",
+        text: "from: " + req.query.from + " concern: " + req.query.subject
+    }, {
+        success: function(httpResponse) {
+            console.log(httpResponse);
+            response.success("Success!");
+        },
+        error: function(httpResponse) {
+            console.error(httpResponse);
+            response.error("Uh oh, something went wrong");
+        }
+    });
 });
 
 // // Example reading from the request query string of an HTTP get request.
